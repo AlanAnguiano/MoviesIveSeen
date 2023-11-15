9e212422e2e24efdd9e72dd46e7a78783607c17c
Alan Anguiano <alan.anguiano@valtre.com>
2023-11-15 00:30:36 -0600

    Add resources for 404 page

---
+++ quotes.js
@@ -0,0 +1,36 @@
+const quotes = [
+  {
+    quote: "My precious.",
+    movie: "The Lord of the Rings: Two Towers, 2002"
+  },
+  {
+    quote: "Houston, we have a problem.",
+    movie: "Apollo 13, 1995"
+  },
+  {
+    quote: "Toto, I've a feeling we're not in Kansas anymore.",
+    movie: "The Wizard of Oz, 1939"
+  },
+  {
+    quote: "Keep your friends close, but your enemies closer.",
+    movie: "The Godfather Part II, 1974"
+  },
+  {
+    quote: "Magic Mirror on the wall, who is the fairest one of all?",
+    movie: "Snow White and the Seven Dwarfs, 1937"
+  },
+  {
+    quote: "All the ones I've loved are now ghosts. But I will fight to keep their memory alive forever.",
+    movie: "Rambo: Last Blood, 2019"
+  },
+  {
+    quote: "The rest of those who have gone before us cannot steady the unrest of those to follow.",
+    movie: "Finding Forrester, 200"
+  },
+  {
+    quote: "With love comes loss, son. It's part of the deal. Sometimes it hurts, but, in the end, it's all worth it. There's no greater gift than love.",
+    movie: "How to Train Your Dragon: The Hidden World, 2019"
+  }
+]
+
+export default quotes
